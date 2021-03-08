fa5e9e5a80376b79d5e77431e5a65ced7e6078e5
w987852 <w987852@gmail>
2021-03-09 06:33:22 +0800

    fix typo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'URL_Shortener' + process.env.NODE_ENV || 'dev',
+      name: 'URL_Shortener-' + process.env.NODE_ENV || 'dev',
       script: 'server.js',
       node_args: '--max_old_space_size=4096',
       watch: [
