439ed1e538774dbf3077a5eea2d7242589017489
w987852 <w987852@gmail>
2021-03-09 07:11:33 +0800

    refactor code, remove unuse code

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var logger = require('morgan');
 
 var app = express();
 const shortenUrlRouter = require('./app/routes/shortenUrl');
-const indexRouter = require('./app/routes/index');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,11 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//global._ = require('lodash');
-//global.ObjectId = require('mongoose').Types.ObjectId;
 
-app.use('/', indexRouter);
-app.use('/shortenUrl', shortenUrlRouter);
+app.use('/', shortenUrlRouter);
 
 
 // catch 404 and forward to error handler
